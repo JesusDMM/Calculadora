086ede376e89b9f28f22a6eca5945c1b8d26bdb7
Chucho <119381852+JesusDMM@users.noreply.github.com>
2023-07-17 17:04:59 -0500

    Se creo el controlador de las excepciones

--- Calculadora.java
+++ Calculadora.java
@@ -1,7 +1,11 @@
 public class Calculadora {
     public static void main(String[] args) {
+       try {
         int numero1 = 2;
         int numero2 = 2;
-        System.out.println(numero1 + numero2);
+        System.out.println(numero1 / numero2);
+       }catch (Exception e){
+        System.out.println(e);
+       }
     }
 }
