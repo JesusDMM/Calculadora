c994afa6aceac5d94575245e46294966326c0ab8
Chucho <119381852+JesusDMM@users.noreply.github.com>
2023-07-17 17:00:49 -0500

    Se agregaron las varibles y se sumaron

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,7 @@
 public class Calculadora {
     public static void main(String[] args) {
-        System.out.println("Hola como estan");
+        int numero1 = 2;
+        int numero2 = 2;
+        System.out.println(numero1 + numero2);
     }
 }
