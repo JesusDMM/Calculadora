39b8820059ec5fb0314e52f6033f4dc3be51ee54
Chucho <119381852+JesusDMM@users.noreply.github.com>
2023-07-17 17:11:44 -0500

    Se creo un menu

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
        try {
-        int numero1 = 2;
-        int numero2 = 2;
-        System.out.println(numero1 / numero2);
+        Scanner leer = new Scanner (System.in);
+        int numero1;
+        int numero2;
+        int numero;
+        System.out.println("Escribe un numero");
+        numero1 = leer.nextInt();
+        numero2 = leer.nextInt();
+        System.out.println("Escribe una operacion");
+        numero = leer.nextInt();
+        switch (numero) {
+            case 1: 
+                System.out.println(numero1 + numero2);
+            break;
+            case 2:
+                System.out.println(numero1 - numero2);
+            break;
+        }
        }catch (Exception e){
         System.out.println(e);
        }
